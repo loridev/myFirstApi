7bdde7495fa0d5617c19be740f600d58b6b5f3c0
loridev <79363993+loridev@users.noreply.github.com>
2021-10-05 18:57:57 +0200

    process env port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const myFirstController = require('./controllers/myFirstController');
 // REQUEST, RESPONSE
 app.get('/holamundo', myFirstController.helloWorld);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("My first API running!");
 });
