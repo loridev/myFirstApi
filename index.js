ac9e392f51d41eff6c542f09295cbe0816ec0e82
loridev <79363993+loridev@users.noreply.github.com>
2021-10-05 19:27:03 +0200

    alumnes API

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 require('dotenv');
 const app = express();
-const appAlumnes = express();
 
 const myFirstController = require('./controllers/myFirstController');
 // REQUEST, RESPONSE
 app.get('/holamundo', myFirstController.helloWorld);
 
-appAlumnes.get('/alumnes', myFirstController.alumnes);
+app.get('/alumnes', myFirstController.alumnes);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("My first API running!");
