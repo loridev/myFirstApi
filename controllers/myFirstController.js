f96f751c70a556c73123b41a5aa1b50de83564fa
loridev <79363993+loridev@users.noreply.github.com>
2021-10-05 19:25:30 +0200

    Array alumnes API

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -3,3 +3,13 @@ module.exports.helloWorld = (req, res) => {
     const status = 200;
     res.status(status).send(jsonResponse);
 }
+
+module.exports.alumnes = (req, res) => {
+    const arrayAlumnes = ["Miquel", "Eric", "Alba", "Marc", "Albert", "Alejandro", "Biel", "Dani", "Gerard", "Gabriel", "Marcos"];
+    const lengthArr = arrayAlumnes.length;
+    const jsonResponse = {
+        alumnesArr: arrayAlumnes,
+        alumnesLength: lengthArr,
+    };
+    res.status(200).send(jsonResponse);
+}
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const myFirstController = require('./controllers/myFirstController');
 // REQUEST, RESPONSE
 app.get('/holamundo', myFirstController.helloWorld);
 
+app.get('/alumnes', myFirstController.alumnes);
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("My first API running!");
 });
