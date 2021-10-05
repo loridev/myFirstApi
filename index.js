e09cac330b8f9518f644864f5ba319b78427e53a
loridev <79363993+loridev@users.noreply.github.com>
2021-10-05 19:26:21 +0200

    e

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 require('dotenv');
 const app = express();
+const appAlumnes = express();
 
 const myFirstController = require('./controllers/myFirstController');
 // REQUEST, RESPONSE
 app.get('/holamundo', myFirstController.helloWorld);
 
-app.get('/alumnes', myFirstController.alumnes);
+appAlumnes.get('/alumnes', myFirstController.alumnes);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("My first API running!");
