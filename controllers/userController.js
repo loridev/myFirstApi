2d7b94c839961cb42d247eaf0224498cd61a0c20
loridev <79363993+loridev@users.noreply.github.com>
2021-10-19 19:16:24 +0200

    Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,28 @@
-const users = [{name: 'john'}, {name: 'david'}, {name: 'maria'}];
+const users = [{id: 1, name: 'john'}, {id: 2, name: 'david'}, {id: 3, name: 'maria'}];
 const status = {
     success: 200,
+    notFound: 404,
 };
 
 module.exports = {
     list: (req, res) => {
-        console.log(req.query.orden); //ACCEDER QUERY PARAMS
+
+         //ACCEDER QUERY PARAMS
         //PETICIO BBDD
         res.status(status.success).send(users);
+    },
+    profile: (req, res) => {
+        //PETICIO BBDD
+
+        const user = users.find((user) => user.id == req.params.userId);
+
+        if (user) {
+            const user = users.find((user) => user.id == req.params.userId);
+        } else {
+            res.status(status.notFound).send({error: 'User not found'});
+        }
+        console.log(req.params);
+
+        res.status(status.success).send(user);
     }
 };
