71b89e8349816ecff2f1bf8b36d1e6980b517352
loridev <79363993+loridev@users.noreply.github.com>
2021-10-24 22:17:08 +0200

    Exercici 2

--- controllers/s04-05Controller.js
+++ controllers/s04-05Controller.js
@@ -1,6 +1,12 @@
+const status = {
+    success: 200,
+    notFound: 404,
+    badRequest: 400
+};
+
 module.exports = {
     milisegundos: (req, res) => {
-        res.status(200).send({dataActualMilisecs: new Date().getTime()});
+        res.status(status.success).send({dataActualMilisecs: new Date().getTime()});
     },
     fechaActual: (req, res) => {
         const dataActual = new Date();
@@ -13,12 +19,12 @@ module.exports = {
         if (mm < 10) {
             mm = '0' + mm;
         }
-        res.status(200).send({dataActual: `${dd}-${mm}-${yyyy}`});
+        res.status(status.success).send({dataActual: `${dd}-${mm}-${yyyy}`});
     },
     horaActual: (req, res) => {
         const dataActual = new Date();
         let hr = dataActual.getHours();
-        let min = dataActual.getMinutes() + 1;
+        let min = dataActual.getMinutes();
         let seg = dataActual.getSeconds();
         if (hr < 10) {
             hr = '0' + hr;
@@ -29,6 +35,21 @@ module.exports = {
         if (seg < 10) {
             seg = '0' + seg;
         }
-        res.status(200).send({horaActual: `${hr}:${min}:${seg}`});
+        res.status(status.success).send({horaActual: `${hr}:${min}:${seg}`});
+    },
+    tablaMult: (req, res) => {
+        const numero = req.params.numero;
+        const message = [];
+
+        if((numero < 2 || numero > 5) || isNaN(numero)) {
+            res.status(status.badRequest).send({error: 'Debes introducir un número entre el 2 y el 5!'});
+        } else {
+            for (let i = 1; i <= 10; i++) {
+                message.push(`${numero} x ${i} = ${numero * i}`);
+            }
+
+            res.status(status.success).send({tabla: message});
+        }
+
     }
 }
--- routes/s04-05Routes.js
+++ routes/s04-05Routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const exercicisController = require('../controllers/s04-05Controller');
 
+//Ex1
 router.get('/ex1-1_milisegundos', exercicisController.milisegundos);
 router.get('/ex1-2_fechaactual', exercicisController.fechaActual);
 router.get('/ex1-3_horaactual', exercicisController.horaActual);
 
-
+//Ex2
+router.get('/ex2_tablamult/:numero', exercicisController.tablaMult);
 
 module.exports = router;
